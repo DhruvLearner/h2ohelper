64160aa524e3911d9b40f46723c3469486bcff1a
mulanidhruvin <mulanidhruvin@gmail.com>
2023-12-13 19:05:18 -0500

    Social Sharing features added

--- screens/Setting Screen/setting_screen.js
+++ screens/Setting Screen/setting_screen.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, Share } from 'react-native'
 import React from 'react'
 import { styles } from './styles'
 import { Entypo } from '@expo/vector-icons';
@@ -6,8 +6,8 @@ import Colors from '../../colors'
 import { useNavigation } from '@react-navigation/native'
 import { useSelector } from 'react-redux';
 
-export default function SettingScreen() {
 
+export default function SettingScreen() {
   const navigation = useNavigation();
   const user = useSelector((state) => state.setting.user);
 
@@ -19,12 +19,34 @@ export default function SettingScreen() {
     navigation.navigate('NotificationScreen');
   }
 
+  const options = {
+    title: 'H2O Helper',
+    message: 'A water reminder application',
+    url: 'https://google.com'
+  }
+
+  const onShare = async () => {
+    try {
+      const result = await Share.share(options);
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+        } else {
+        }
+      } else if (result.action === Share.dismissedAction) {
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
+
+
+
   return (
     <View style={styles.container}>
-        <View style={styles.nameView}>
-          <Text style={styles.nameStyle}> { user?.name || 'Update Profile' }</Text>
-          <Text style={styles.genderTextStyle}> {user?.gender}</Text>
-        </View>
+      <View style={styles.nameView}>
+        <Text style={styles.nameStyle}> {user?.name || 'Update Profile'}</Text>
+        <Text style={styles.genderTextStyle}> {user?.gender}</Text>
+      </View>
       <TouchableOpacity onPress={handleUpdateProfileClick}>
         <View style={styles.option}>
           <Text style={styles.optionTextStyle}>Update Profile</Text>
@@ -37,7 +59,7 @@ export default function SettingScreen() {
           <Entypo name="chevron-right" size={24} color={Colors.primaryColor} />
         </View>
       </TouchableOpacity>
-      <TouchableOpacity onPress={()=>{ navigation.navigate('HydrationTips'); }}>
+      <TouchableOpacity onPress={() => { navigation.navigate('HydrationTips'); }}>
         <View style={styles.option}>
           <Text style={styles.optionTextStyle}>Hydration Tips</Text>
           <Entypo name="chevron-right" size={24} color={Colors.primaryColor} />
@@ -55,7 +77,9 @@ export default function SettingScreen() {
           <Entypo name="chevron-right" size={24} color={Colors.primaryColor} />
         </View>
       </TouchableOpacity>
-      <TouchableOpacity onPress={handleUpdateProfileClick}>
+      <TouchableOpacity onPress={async () => {
+         await onShare();
+      }}>
         <View style={styles.option}>
           <Text style={styles.optionTextStyle}>Share H2OHelper</Text>
           <Entypo name="chevron-right" size={24} color={Colors.primaryColor} />
